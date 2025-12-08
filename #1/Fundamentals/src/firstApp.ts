6fd589934fd6d7d01c1f666165a2899e7b88898d
Symon <fritzsymon23@gmail.com>
2025-12-08 16:12:51 +0800

    Explicit and Implicit Returns

--- #1/Fundamentals/src/firstApp.ts
+++ #1/Fundamentals/src/firstApp.ts
@@ -2,8 +2,14 @@
 
 // console.log("Hello", name);
 
+// Explicit Return
 function calculateTotal(price: number, quantity: number): number {
   return price + quantity;
 }
 
+function userPrompt(say1: string, prompt: string) {
+  return `${say1} & ${prompt}`;
+}
+
 console.log(calculateTotal(10, 50));
+console.log(userPrompt("Tae", "Saur"));
