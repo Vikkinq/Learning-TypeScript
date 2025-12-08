f55718d893d978bb49fb1290a499ac646db8ecf4
Symon <fritzsymon23@gmail.com>
2025-12-08 15:52:10 +0800

    First Commit | Learning TypeScript

---
+++ #1/Fundamentals/src/firstApp.ts
@@ -0,0 +1,9 @@
+// const name: string = "Symon";
+
+// console.log("Hello", name);
+
+function calculateTotal(price: number, quantity: number): number {
+  return price + quantity;
+}
+
+console.log(calculateTotal(10, 50));
