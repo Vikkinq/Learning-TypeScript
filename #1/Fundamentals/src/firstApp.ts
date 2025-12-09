c069ab9db84d1cb094bf9fd516876e3e58fbed43
Symon <fritzsymon23@gmail.com>
2025-12-09 21:22:38 +0800

    Void Return Types

--- #1/Fundamentals/src/firstApp.ts
+++ #1/Fundamentals/src/firstApp.ts
@@ -7,8 +7,13 @@ function calculateTotal(price: number, quantity: number): number {
   return price + quantity;
 }
 
+// Implicit Returns
 function userPrompt(say1: string, prompt: string) {
   return `${say1} & ${prompt}`;
+}
+
+function testingPrompt(g1: string, g2: string): void {
+  console.log(`${g1} & ${g2}`);
 }
 
 console.log(calculateTotal(10, 50));
