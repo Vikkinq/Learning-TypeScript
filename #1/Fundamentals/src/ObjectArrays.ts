4b1fa4a3452d95dd6c2f1980a12b23cf56694b28
Symon <fritzsymon23@gmail.com>
2025-12-10 21:47:09 +0800

    Object Arrays for TypeScript

--- #1/Fundamentals/src/ObjectArrays.ts
+++ #1/Fundamentals/src/ObjectArrays.ts
@@ -2,3 +2,25 @@ type newUser = {
   username: string;
   password: string;
 };
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  inStock: boolean;
+};
+
+const products: Product[] = [
+  {
+    id: 101,
+    title: "Samsung Phone",
+    price: 15000,
+    inStock: true,
+  },
+  {
+    id: 126,
+    title: "Samsung Odyssey Monitor",
+    price: 45000,
+    inStock: true,
+  },
+];
